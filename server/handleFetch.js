1d81a97e302dbc8ea44a66c25635ccc550272a3d
Jordit <jordidiaz2020@gmail.com>
2025-04-03 20:21:21 -0400

    Setup env variables

--- server/handleFetch.js
+++ server/handleFetch.js
@@ -1,12 +1,15 @@
-export const handleFetch = async (url, options = {}) => {
+const handleFetch = async (url, options = {}) => {
   try {
     const response = await fetch(url, options);
     const { ok, status, statusText, headers } = response;
 
-    if (!ok) throw new Error(`Fetch failed with status - ${status}, ${statusText}`);
+    if (!ok)
+      throw new Error(`Fetch failed with status - ${status}, ${statusText}`);
 
     // figure out the type of response (JSON or non-JSON)
-    const isJson = (headers.get('content-type') || '').includes('application/json');
+    const isJson = (headers.get("content-type") || "").includes(
+      "application/json"
+    );
 
     // use response.json() for JSON responses and response.text() for all other content types
     const responseData = await (isJson ? response.json() : response.text());
@@ -14,9 +17,10 @@ export const handleFetch = async (url, options = {}) => {
     // return a "tuple" representing [data, error]. Here the data exists and the error is null.
     return [responseData, null];
   } catch (error) {
-    console.warn(error)
-
+    console.warn(error);
     // return a "tuple" representing [data, error]. Here the data is null and the error exists.
     return [null, error];
   }
 };
+
+module.exports = { handleFetch };
--- server/index.js
+++ server/index.js
@@ -2,15 +2,18 @@
 // Imports
 //////////////////////////
 
-const path = require('path');
-const express = require('express');
+const { handleFetch } = require("./handleFetch");
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 //////////////////////////
 // Constants
 //////////////////////////
 
 const port = 8080;
-const pathToDistFolder = path.join(__dirname, '../frontend/dist');
+const pathToDistFolder = path.join(__dirname, "../frontend/dist");
 const app = express();
 
 //////////////////////////
@@ -21,8 +24,22 @@ const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
 
+const serveGifs = async (req, res, next) => {
+  const [data, error] = await handleFetch(
+    `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`
+  );
+  if (data) res.send(data);
+  if (error) res.status(503).send(error);
+};
+
+//////////////////////////
+// Endpoints
+//////////////////////////
+
+app.get("/api/gifs", serveGifs);
+
 //////////////////////////
 // Listener
 //////////////////////////
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
